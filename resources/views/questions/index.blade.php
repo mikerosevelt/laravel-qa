2543d4c22a5f750a4df113bc1db1822079083055
mikerosevelt <zoe_abbas@aim.com>
2020-04-03 14:09:48 +0700

    Add delete question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -34,7 +34,12 @@
                             <div class="d-flex align-items-center">
                                 <h3 mt-0><a href="{{ $question->url }}">{{$question->title}}</a></h3>
                                 <div class="ml-auto">
-                                    <a href="{{ route('questions.edit', $question->id) }} " class="btn btn-sm btn-outline-info">Edit</a>
+                                    <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
+                                    <form class="form-delete" action="{{ route('questions.destroy', $question->id) }}" method="POST">
+                                        @method('DELETE')
+                                        @csrf
+                                        <button class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                    </form>
                                 </div>
                             </div>
                             <p class="lead">
